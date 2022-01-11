d99759b35cc1366d745901ba59256736a5075d8d
Eric Li <ericli@knights.ucf.edu>
2022-01-10 23:15:54 -0700

    Refactor Links and test liking function

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,23 +1,26 @@
 import './ImageCard.css';
+import emptyHeart from '../../images/emptyHeart.svg';
 import { Link } from 'react-router-dom';
 
-const ImageCard = ({ singleImgData }) => {
+const ImageCard = ({ singleImgData, toggleImageLike }) => {
 
-  const { id, date, title, copyright, synopsis, imgUrl } = singleImgData;
+  const { id, date, title, imgUrl, isLiked } = singleImgData;
 
   return (
-    <Link to={`/${id}`} className="image-card-link">
-      <article className="image-card">
+    <article className="image-card">
       <header className="image-card-header">
-        <img className="image-card-photo" src={imgUrl} alt={title}>
-        </img>
+        <Link to={`/${id}`} className="image-card-link">
+          <img className="image-card-photo" src={imgUrl} alt={title}>
+          </img>
+        </Link>
       </header>
       <footer className="image-card-footer">
+        {isLiked ? <p>Liked</p> : <img className="unliked-icon" alt="Unliked icon" src={emptyHeart}></img>}
         <h1 className="image-card-title">{title}</h1>
         <p className="image-card-date">{date}</p>
+        <button className="detail-card-like" onClick={() => toggleImageLike(id)}>LIKE</button>
       </footer>
-      </article>
-    </Link>
+    </article>
   );
 }
 
