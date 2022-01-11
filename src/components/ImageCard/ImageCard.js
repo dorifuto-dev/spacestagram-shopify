4cdbf04d16853d9ee3f0e3a6bb64561bdc69dde2
Eric Li <ericli@knights.ucf.edu>
2022-01-10 20:13:33 -0700

    Add styling, semantic JSX, setup Links

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,21 +1,23 @@
 import './ImageCard.css';
+import { Link } from 'react-router-dom';
 
 const ImageCard = ({ singleImgData }) => {
 
   const { id, date, title, copyright, synopsis, imgUrl } = singleImgData;
 
   return (
-    <article className="image-card">
+    <Link to={`/${id}`} className="image-card-link">
+      <article className="image-card">
       <header className="image-card-header">
         <img className="image-card-photo" src={imgUrl} alt={title}>
         </img>
+      </header>
+      <footer className="image-card-footer">
         <h1 className="image-card-title">{title}</h1>
         <p className="image-card-date">{date}</p>
-      </header>
-      <footer>
-        <p className="image-card-description">{synopsis}</p>
       </footer>
-    </article>
+      </article>
+    </Link>
   );
 }
 
