885c56b914741024c5e90db49902160fac23850b
Eric Li <ericli@knights.ucf.edu>
2022-01-12 15:41:09 -0700

    Create sample data file

---
+++ src/sampleAPODdata.js
@@ -0,0 +1,65 @@
+export const sampleNasaData = [
+  {
+    id: 20211101,
+    date: "2021-11-01",
+    title: "Moonrise on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211031,
+    date: "2021-10-31",
+    title: "Breakfast on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211030,
+    date: "2021-10-30",
+    title: "Lunch on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211029,
+    date: "2021-10-29",
+    title: "Dinner on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211028,
+    date: "2021-10-28",
+    title: "Desert on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211027,
+    date: "2021-10-27",
+    title: "Dessert on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  },
+  {
+    id: 20211026,
+    date: "2021-10-26",
+    title: "Amuse Bouche on Mars",
+    copyright: "Eric Li",
+    synopsis: "Dolorem aspernatur sint et et cupiditate eum consequatur natus sapiente qui beatae deleniti error reiciendis eligendi dolor velit et laborum. Rem sed minus debitis ipsum voluptates consequuntur autem explicabo eos iure et quasi fugiat recusandae doloribus est sint ducimus vel. Placeat ratione nostrum in sit at nesciunt culpa iste officia labore et dolor unde voluptatum aspernatur omnis asperiores consequatur eius.",
+    imgUrl: "https://miro.medium.com/max/6200/1*teBtR_0pirBnX4nURoMvLA.jpeg",
+    isLiked: false
+  }
+]
