b2459218fa3d4d48db9c835c9c9af010fb642eb7
Eric Li <ericli@knights.ucf.edu>
2022-01-12 15:42:01 -0700

    Style initial DetailCard component

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -1,4 +1,7 @@
 import './DetailCard.css';
+import emptyHeart from '../../images/emptyHeart.svg';
+import redHeart from '../../images/redHeart.svg';
+import backArrow from '../../images/backArrow.svg';
 import { Link, useParams } from 'react-router-dom';
 
 const DetailCard = ({ imageData, toggleImageLike }) => {
@@ -9,23 +12,30 @@ const DetailCard = ({ imageData, toggleImageLike }) => {
     return JSON.stringify(image.id) === id
   });
 
-  const { title, copyright, date, synopsis, imgUrl, isLiked } = currentImage && currentImage
+  const { title, copyright, date, synopsis, imgUrl, isLiked } = currentImage ? currentImage : ""
 
   return (
-    <article className="detail-card">
-      <Link to="/">Back to Home</Link>
+    <article className="detail-card"> 
       { currentImage && 
         <>
-          { isLiked ? <p>Liked</p> : <p>Not Liked</p> }
           <header className="detail-card-header">
             <img className="detail-card-image" alt={title} src={imgUrl}></img>
+            <div className="detail-buttons-container">
+              { isLiked ? 
+                <button className="detail-like-button" onClick={() => toggleImageLike(id)}>
+                  <img className="detail-liked-icon" alt="Liked icon" src={redHeart}></img>
+                </button> : 
+                <button className="detail-like-button" onClick={() => toggleImageLike(id)}>
+                  <img className="detail-unliked-icon" alt="Unliked icon" src={emptyHeart}></img>
+                </button> }
+              <Link to="/"><img className="go-back-icon" alt="Go back button" src={backArrow}></img></Link>
+            </div> 
           </header>
           <footer className="detail-card-text">
             <h1 className="detail-card-title">{title}</h1>
-            <p className="detail-card-copyright">{copyright}</p>
+            { copyright && <p className="detail-card-copyright">{`Photo by: ${copyright}`}</p>}
             <p className="detail-card-date">{date}</p>
             <p className="detail-card-synopsis">{synopsis}</p>
-            <button className="detail-card-like" onClick={() => toggleImageLike(id)}>LIKE</button>
           </footer>
         </> 
       }
