c7117533128dfd71a27c59e63e36a0661d4626fa
Eric Li <ericli@knights.ucf.edu>
2022-01-14 18:15:29 -0700

    Correctly stub GET request

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -2,14 +2,9 @@ describe('Spacestagram - User Flows', () => {
 
   beforeEach(() => {
     
-    cy.intercept('https://api.nasa.gov/planetary/apod?start_date=2021-12-11&end_date=2021-12-25&api_key=MEDNcZSPhOUJrNsjUhQdxSthGKcS3Pe0qeIx3OdR**')
+    cy.intercept('GET', 'https://api.nasa.gov/planetary/apod**', {fixture: 'images.json'})
     cy.visit('http://localhost:3000/')
-       // cy.intercept('GET', 'https://api.nasa.gov/planetary/apod?start_date=2021-12-01&end_date=2021-12-25&api_key=MEDNcZSPhOUJrNsjUhQdxSthGKcS3Pe0qeIx3OdR', 
-    //   { 
-    //     statusCode: 201,
-    //     fixture: 'images.json' 
-    //   }
-    // )
+    
   })
 
   it('Should have a default homepage URL', () => {
@@ -75,3 +70,5 @@ describe('Spacestagram - User Flows', () => {
       .should('not.exist')
   })
 })
+
+// cy.intercept('https://api.nasa.gov/planetary/apod?start_date=2021-12-11&end_date=2021-12-25&api_key=MEDNcZSPhOUJrNsjUhQdxSthGKcS3Pe0qeIx3OdR**')
